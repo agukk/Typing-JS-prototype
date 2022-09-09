a61bd51d2431d031d9d968d77343463310f2f97c
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-09-09 16:09:53 +0900

    first commit

---
+++ js/paragraphs.js
@@ -0,0 +1,13 @@
+const paragraphs = [
+    "There has been a tendency in many countries over recent years for more people of all ages to choose to live alone.", 
+    "This essay will discuss the reasons for this and explain why this can have both advantages and disadvantages for society.", 
+    "One reason for this trend is economic. People are generally more affluent than in the past, and this means that they can afford to make the choice to live alone, something not always possible in the past.", 
+    "In addition to this, there are also cultural factors. There used to be more pressure to marry young and think about having a family.", 
+    "Now though, people desire self-fulfilment, and will marry later or divorce if their marriage is not happy.", 
+    "Also, the developments in communication technology such as social media mean that people can live alone but still feel connected to others.",  
+    "I would argue that this development has positive and negative impacts on society.", 
+    "A positive impact is that those individuals who are young and single have helped to revitalise cities around the world, as they are more likely to live in central locations and socialise, spend money and participate in public life than those living with others.",
+    "However, a drawback is that some people living alone who experience problems may not have an outlet to talk about them.", 
+    "This lack of social support could lead to more mental health problems in the general population, which would need to be dealt with by public health care services.",  
+    "To conclude, economic and cultural factors, and changes in communication have resulted in more people living alone, and, although this has benefits to society, there are also negative impacts."
+]
---
+++ js/script.js
@@ -0,0 +1,84 @@
+const typingText = document.querySelector(".typing-text p");
+let inpField = document.querySelector(".wrapper .input-field");
+let correctTag = document.querySelector(".correct-types span");
+let mistakeTag = document.querySelector(".miss-types span");
+let accuracyTag = document.querySelector(".accuracy span");
+let tryAgainBtn = document.querySelector("button");
+
+let charIndex = correctTypes = mistakes = 0;
+
+function randomParagraph() {
+    // getting random number and it'll always less than the paragraphs length
+    let randIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    // getting random item from the paragraphs array, splitting all characters of it,
+    // adding each character inside span and the adding this span inside p tag.
+    paragraphs[randIndex].split("").forEach((char) => {
+        let spanTag = `<span>${char}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    // focusing input field on keydown or click event
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+
+    // if user hasn't entered any character or pressed backspace
+    if(typedChar == null)
+    {
+        charIndex--; // decrement charIndex
+        // decrement mistakes and correctTypes only if the charIndex span contains incorrect class and correct class
+        if(characters[charIndex].classList.contains("incorrect"))
+        {
+            mistakes--;
+        }
+        else
+        {
+            correctTypes--;
+        }
+        characters[charIndex].classList.remove("correct", "incorrect");
+    }
+    else
+    {
+        if(characters[charIndex].innerHTML === typedChar)
+        {
+            // if user typed character and shown character matched, increment the correctTypes and add the correct class 
+            correctTypes++
+            characters[charIndex].classList.add("correct");
+        }
+        else 
+        {
+            // if user typed character and shown character unmatched, increment the mistakes and add the incorrect class
+            mistakes++;
+            characters[charIndex].classList.add("incorrect");
+        }
+        charIndex++; // increment charIndex either user typed correct or incorrect character
+    }
+    characters.forEach((char) => char.classList.remove("active"));
+    characters[charIndex].classList.add("active");
+
+    const accuracy = (correctTypes / (correctTypes + mistakes) * 100).toFixed(1)
+
+    correctTag.innerHTML = correctTypes;
+    mistakeTag.innerHTML = mistakes;
+    accuracyTag.innerHTML = accuracy;
+}
+
+function resetTyping() {
+    // calling randomParagraph function and reseting each variables and elements value to default
+    randomParagraph();
+    inpField.value = "";
+    charIndex = correctTypes = mistakes = 0;
+    correctTag.innerHTML = 0;
+    mistakeTag.innerHTML = 0;
+    accuracyTag.innerHTML = "";
+}
+
+randomParagraph();
+
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetTyping)
